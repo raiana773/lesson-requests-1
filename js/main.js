944faa8f071df4dfe7fa0a229bcc309b3eb9fb84
raiana773 <104357057+raiana773@users.noreply.github.com>
2022-06-07 12:44:35 +0545

    added get request

--- js/main.js
+++ js/main.js
@@ -11,3 +11,64 @@ GET - получение данных
 Команды для запроса json-server
 json-server -w db.json -p 8000
  */
+
+/* 
+GRUD- Create(POST - request) Read(GET - request) Update(PUT/PATCH - request) Delete(DELETE - request)
+*/
+const API = "http://localhost:8000/todo";
+
+// ! Create
+//  получаем нужные для добавления элементы
+let inpAdd = document.querySelector("#inp-add");
+let btnAdd = document.querySelector("#btn-add");
+// console.log(inpAdd, btnAdd);
+
+//  навесили событие на кнопку "сохранить"
+btnAdd.addEventListener("click", async function () {
+  //  собираем объект для добавления в дб.жсон
+  let newTodo = {
+    todo: inpAdd.value,
+  };
+  // console.log(newTodo);
+  //  проверка на заполненность инпута и останавливаем код с помощъю return, пост-запрос не выполнился
+  if (newTodo.todo.trim() === "") {
+    alert("запомните поля!");
+    return;
+  }
+  //  запрос для добавления
+  await fetch(API, {
+    method: "POST", //  указываем метод
+    body: JSON.stringify(newTodo), // указываем что именно нужно запостить
+    headers: {
+      "Content-type": "application/json; charset=utf-8",
+    }, //  кодировка
+  });
+  //  очищяем инпут после добавления
+  inpAdd.value = "";
+  //  чтоб добавленный таск сразу отобразился в листе вызываем функцию которая выполняет отображение
+  getTodos();
+});
+
+// ! Read
+//  получаем элемент, чтоб в нем отобразить все таски
+let list = document.getElementById("list");
+// проверяем в консоли, чтоб убедиться, что в переменной list сейчас НЕ пусто
+console.log(list);
+// Функция для получения всех тасков и отображения их в div#list
+//  async await нужен здесь, чтоб при отправке запроса мы сначала получили данные и только потом записали все в переменную response, иначе (если мы Не дождемся) туда запишется pending (состояние промиса, еоторый еще не выполнен)
+async function getTodos() {
+  let response = await fetch(API) //   если не указать метод запроса,  то получим это GET запрос
+    .then(res => res.json()) //   переводим все в json формат
+    .catch(err => console.log(err)); //  отловили ошибку
+  console.log(response);
+  //  очищяем div#list, чтоб список тасков корректно отображался и не хранил там предыдущие html-элементы со старыми данными
+  list.innerHTML = "";
+  //  перебираем полученный из дб.жсон массив и для каждого объекта из этого массива создаем div и задаем ему содержимое через метод innerHTML, каждый созданный элемент аппендим в div#list
+  response.forEach(item => {
+    let newElem = document.createElement("div");
+    newElem.innerHTML = `<span>${item.todo}</span>`;
+    list.append(newElem);
+  });
+}
+//  вызываем функцию, чтоб как только откроется страница что-то было отображено
+getTodos();
