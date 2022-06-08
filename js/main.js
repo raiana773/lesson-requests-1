075699f47883924ea17938d85afa218f5e85404e
raiana773 <104357057+raiana773@users.noreply.github.com>
2022-06-08 12:58:33 +0545

    added delete

--- js/main.js
+++ js/main.js
@@ -53,22 +53,37 @@ btnAdd.addEventListener("click", async function () {
 //  получаем элемент, чтоб в нем отобразить все таски
 let list = document.getElementById("list");
 // проверяем в консоли, чтоб убедиться, что в переменной list сейчас НЕ пусто
-console.log(list);
+// console.log(list);
 // Функция для получения всех тасков и отображения их в div#list
 //  async await нужен здесь, чтоб при отправке запроса мы сначала получили данные и только потом записали все в переменную response, иначе (если мы Не дождемся) туда запишется pending (состояние промиса, еоторый еще не выполнен)
 async function getTodos() {
   let response = await fetch(API) //   если не указать метод запроса,  то получим это GET запрос
     .then(res => res.json()) //   переводим все в json формат
     .catch(err => console.log(err)); //  отловили ошибку
-  console.log(response);
+  // console.log(response);
   //  очищяем div#list, чтоб список тасков корректно отображался и не хранил там предыдущие html-элементы со старыми данными
   list.innerHTML = "";
   //  перебираем полученный из дб.жсон массив и для каждого объекта из этого массива создаем div и задаем ему содержимое через метод innerHTML, каждый созданный элемент аппендим в div#list
   response.forEach(item => {
     let newElem = document.createElement("div");
-    newElem.innerHTML = `<span>${item.todo}</span>`;
+    newElem.id = item.id;
+    newElem.innerHTML = `
+    <span>${item.todo}</span>
+    <button class='btn-delete'>Delete</button>`;
     list.append(newElem);
   });
 }
 //  вызываем функцию, чтоб как только откроется страница что-то было отображено
 getTodos();
+
+document.addEventListener("click", async function (e) {
+  if (e.target.className === "btn-delete") {
+    let id = e.target.parentNode.id;
+    await fetch(`${API}/${id}`, {
+      method: "DELETE",
+    });
+    getTodos();
+  }
+  // console.log(event.target.className);
+  // console.log(event.target.parentNode.id);
+});
